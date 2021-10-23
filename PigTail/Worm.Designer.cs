a2fe27a96f42cd7b98c1d67c624f275ecd8c7c1f
pekkasuper <3062055813@qq.com>
2021-10-24 01:28:54 +0800

    获取公开uuid并显示窗体上传

---
+++ PigTail/Worm.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace PigTail
+{
+    partial class Worm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.random = new System.Windows.Forms.Button();
+            this.uuidBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // random
+            // 
+            this.random.Location = new System.Drawing.Point(341, 149);
+            this.random.Name = "random";
+            this.random.Size = new System.Drawing.Size(78, 25);
+            this.random.TabIndex = 0;
+            this.random.Text = "随机UUID";
+            this.random.UseVisualStyleBackColor = true;
+            this.random.Click += new System.EventHandler(this.random_Click);
+            // 
+            // uuidBox
+            // 
+            this.uuidBox.Location = new System.Drawing.Point(90, 149);
+            this.uuidBox.Name = "uuidBox";
+            this.uuidBox.Size = new System.Drawing.Size(233, 25);
+            this.uuidBox.TabIndex = 1;
+            this.uuidBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.uuidBox_KeyPress);
+            // 
+            // Worm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PigTail.Properties.Resources.worm;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(462, 223);
+            this.Controls.Add(this.uuidBox);
+            this.Controls.Add(this.random);
+            this.MaximizeBox = false;
+            this.Name = "Worm";
+            this.Text = "获取公开对局uuid";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button random;
+        private System.Windows.Forms.TextBox uuidBox;
+    }
+}
---
+++ PigTail/Worm.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PigTail
+{
+    public partial class  Worm: Form
+    {
+        public Worm()
+        {
+            InitializeComponent();
+        }
+
+        private void uuidBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.uuidBox.ReadOnly = true;
+        }
+
+        /// <summary>
+        /// 获取随机的uuid，由于技术能力限制仅能对返回的uuid列表采用
+        /// 搭配随机数的方式随机获取uuid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void random_Click(object sender, EventArgs e)
+        {
+            string rec = FetchInfo.executeOperation("fetchPublicContest");
+            JObject jo = (JObject)JsonConvert.DeserializeObject(rec);
+            if(jo["code"].ToString() == "200")
+            {
+                MessageBox.Show("操作成功");
+                JArray items = (JArray)jo["data"]["games"];
+                Random r = new Random();
+                int which = r.Next(0, items.Count);
+                this.uuidBox.Text = ((JObject)items[which])["uuid"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("登录失效了，请重新登录");
+            }
+        }
+    }
+}

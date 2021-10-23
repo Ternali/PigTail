b13fb1482e219da5dfcde333d1524d2ab3aaca23
pekkasuper <3062055813@qq.com>
2021-10-24 01:31:05 +0800

    扑克牌花色概率计算类

---
+++ PigTail/Probablity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigTail
+{
+    class Probablity
+    {
+        private double pokerNext;
+        private string pokerType;
+        public Probablity(double pokerNext, string pokerType)
+        {
+            this.pokerNext = pokerNext;
+            this.pokerType = pokerType;
+        }
+        /// <summary>
+        /// 自定义比较函数用于排序可能性
+        /// </summary>
+        /// <param name="one"></param>
+        /// <param name="two"></param>
+        /// <returns></returns>
+        public static int Compare(Probablity one, Probablity two)
+        {
+            if (one.pokerNext < two.pokerNext)return 1;
+            else if (one.pokerNext == two.pokerNext)return 0;
+            else return -1;
+        }
+
+        public string getPokerType()
+        {
+            return this.pokerType;
+        }
+    }
+}
